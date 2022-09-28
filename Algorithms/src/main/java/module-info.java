18ba2d70848b6d5bc3ec080b3a5ef2a3eac6b940
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2022-09-28 20:44:43 +0100

    Delete Algorithms/src/main directory

--- Algorithms/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.algorithms {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.algorithms to javafx.fxml;
-    exports com.example.algorithms;
-}
