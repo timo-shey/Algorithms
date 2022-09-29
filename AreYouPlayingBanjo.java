1185389c5f17778f4d1654d6f843cfa0fc62d26d
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2022-09-29 22:59:36 +0100

    Add files via upload
    A Java algorithm on 'Are you playing banjo'

---
+++ AreYouPlayingBanjo.java
@@ -0,0 +1,20 @@
+package CodeWars;
+
+/*
+Create a function which answers the question "Are you playing banjo?".
+        If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+        The function takes a name as its only argument, and returns one of the following strings:
+
+        name + " plays banjo"
+        name + " does not play banjo"
+        Names given are always valid strings.
+*/
+
+public class AreYouPlayingBanjo {
+    public static String areYouPlayingBanjo(String name) {
+        // Program me!
+        boolean check = name.charAt(0) == 'r' || name.charAt(0) == 'R';
+        return  check ? name + " plays banjo" : name + " does not play banjo";
+    }
+}
