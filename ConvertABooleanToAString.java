29eb87703544e6b87f71690b5713e66ae96fa0b7
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2022-10-09 23:44:06 +0100

    Add files via upload

---
+++ ConvertABooleanToAString.java
@@ -0,0 +1,11 @@
+package CodeWars;
+
+public class ConvertABooleanToAString {
+    public static String convert(boolean b){
+        if(b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
